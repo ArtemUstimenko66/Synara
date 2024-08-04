7f0f2ee4d4f87cea1e86d50f3c85e74241473338
Danil5250 <106326776+Danil5250@users.noreply.github.com>
2024-08-04 21:51:22 +0300

    commit_preparedb_2

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,18 @@
-import { Module } from '@nestjs/common';
+import {Module, OnModuleInit} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
+import {Connection} from "typeorm";
 
 @Module({
   imports: [
       TypeOrmModule.forRoot({
           type: "postgres",
-          host: "",
+          host: "34.116.231.64",
           port: 5432,
-          username: "",
-          password: "",
-          database: "",
+          username: "postgres",
+          password: "bkLpa@:&)<8?7LBJ",
+          database: "Synara",
           entities: [__dirname + "/entities/**/*.entity{.ts,.js}"],
           synchronize : true
       })
@@ -19,4 +20,15 @@ import {TypeOrmModule} from "@nestjs/typeorm";
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+    constructor(private readonly connection: Connection) {}
+
+    async onModuleInit() {
+        try {
+            const result = await this.connection.query('SELECT 1');
+            console.log('Successfully connected to the database.');
+        } catch (error) {
+            console.error('Unable to connect to the database:', error);
+        }
+    }
+}
