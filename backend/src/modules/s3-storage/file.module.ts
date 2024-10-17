c2afccdf16503af9cebcd8d9ab1934c1af2c981f
Artem <artemmmm5454545@gmail.com>
2024-10-17 13:15:47 +0300

    api changes

--- backend/src/modules/s3-storage/file.module.ts
+++ backend/src/modules/s3-storage/file.module.ts
@@ -9,11 +9,22 @@ import { S3Config } from './aws-s3.config';
 import { Announcement } from '../announcement/announcement.entity';
 import { FileGathering } from './entities/file-gathering.entity';
 import { GatheringsModule } from '../gatherings/gatherings.module';
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 @Module({
   imports: [
     TypeOrmModule.forFeature([File, User, Announcement, FileGathering]),
     GatheringsModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRATION') },
+      }),
+    }),
   ],
+
   controllers: [FileController],
   providers: [S3Service, FileService, S3Config],
   exports: [S3Service],
